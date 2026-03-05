b7cb81cd624b2ebbc6dd0a40893e1c214769ae3a
Sssebas81 <jsebastianortiz81@gmail.com>
2026-03-05 07:23:12 -0500

    module game

--- src/games/games.module.ts
+++ src/games/games.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { GamesService } from './games.service';
 import { CommentService } from './comment/comment.service';
 import { CommentModule } from './comment.module';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {Game} from './entities/game.entity';
 
 @Module({
   providers: [GamesService, CommentService],
-  imports: [CommentModule]
+  imports: [TypeOrmModule.forFeature([Game]), CommentModule]
 })
 export class GamesModule {}
