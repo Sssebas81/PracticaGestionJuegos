de61ad294b4c6b85b6068fdd47aeaaae4373dad5
Sssebas81 <jsebastianortiz81@gmail.com>
2026-03-07 21:19:56 -0500

    feat: finish

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,11 @@ import { UsersModule } from './user/user.module';
 import { RolesModule } from './roles/roles.module';
 import { RolePermissionModule } from './rolePermission/role-permission.module';
 import { PermissionModule } from './permission/permission.module';
-import { RolePermissionController } from './rolePermission/role-permission.controller';
+
 
 @Module({
+  
   imports: [UsersModule, RolesModule, RolePermissionModule, PermissionModule],
-  controllers: [RolePermissionController],
   
 })
 export class AuthModule {}
