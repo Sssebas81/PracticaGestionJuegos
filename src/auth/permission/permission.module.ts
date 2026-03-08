5a73102e4608f9fe0f6a1e848e1d7a1abd118b02
Sssebas81 <jsebastianortiz81@gmail.com>
2026-03-07 21:09:28 -0500

    finish mthfk

--- src/auth/permission/permission.module.ts
+++ src/auth/permission/permission.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { PermissionService } from './permission.service';
 import {TypeOrmModule} from '@nestjs/typeorm';
 import {Permission} from '../entities/permission.entity';
+import { PermissionController } from './permission.controller';
 
 @Module({
   providers: [PermissionService],
   imports: [TypeOrmModule.forFeature([Permission])],
-  exports: [PermissionService]
+  exports: [PermissionService],
+  controllers: [PermissionController]
   
 })
 export class PermissionModule {}
---
+++ src/auth/rolePermission/role-permission.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import {RolePermissionService} from './role-permission.service';
+
+@Controller('role-permissions')
+
+export class RolePermissionController {
+
+    constructor( private readonly rolePermissionService: RolePermissionService) {}
+
+    @Get()
+
+    findAll() {
+        return this.rolePermissionService.findAll();
+    }
+
+    @Get(':id')
+    findById(@Param('id') id: string) {
+        return this.rolePermissionService.findById(+id);
+    }
+}
