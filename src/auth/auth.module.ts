c1454a8a87dd2ae1bf03dbb0a51d5bf92b185f54
Sssebas81 <jsebastianortiz81@gmail.com>
2026-03-07 20:59:39 -0500

    error fixed

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './user/user.module';
 import { RolesModule } from './roles/roles.module';
-import { RolePermissionService } from './rolePermission/role-permission.service';
 import { RolePermissionModule } from './rolePermission/role-permission.module';
 import { PermissionModule } from './permission/permission.module';
 
 @Module({
   imports: [UsersModule, RolesModule, RolePermissionModule, PermissionModule],
-  providers: [RolePermissionService]
+  
 })
 export class AuthModule {}
